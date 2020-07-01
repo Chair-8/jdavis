92995cbaed6be6b4fa30c50eba9233d8ee665ccc
Joe Cory <hello@joecory.co.uk>
2020-07-01 22:18:30 +0100

    fix (

--- scripts/team.js
+++ scripts/team.js
@@ -13,7 +13,7 @@ function init(){
     console.log($(e.currentTarget))
     console.log($(e.currentTarget.offsetLeft)[0] + $(e.currentTarget.offsetWidth)[0])
     console.log($(window).width())
-    if($(e.currentTarget.offsetLeft)[0] + $(e.currentTarget.offsetWidth)[0] + $(e.currentTarget.offsetWidth)[0] + parseInt$(e.currentTarget).css('margin-right')) > $('teamfeed').width()){
+    if($(e.currentTarget.offsetLeft)[0] + $(e.currentTarget.offsetWidth)[0] + $(e.currentTarget.offsetWidth)[0] + parseInt($(e.currentTarget).css('margin-right')) > $('teamfeed').width()){
       //lightbox goes to the left
       $(e.currentTarget).find('.teamItem_lightbox').addClass('rightAlign')
     }else{
