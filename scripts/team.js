ae89dde4ca631009f6390ea413ae713e5ad5a523
Joe Cory <hello@joecory.co.uk>
2020-07-01 20:59:20 +0100

    change to host

--- scripts/team.js
+++ scripts/team.js
@@ -1,6 +1,6 @@
 function init(){
   if($('teamfeed').length && $('teamfeed').html() == "")
-  var url = window.location.href + '/team'
+  var url = window.location.host + '/team'
   $.getJSON( url + '?format=json-pretty', function( data ) {
     console.log(data);
   })
