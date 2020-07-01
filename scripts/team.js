49785cdb05c5b2681d90a788b2a0bee906be5fe0
Joe Cory <hello@joecory.co.uk>
2020-07-01 21:00:50 +0100

    a

--- scripts/team.js
+++ scripts/team.js
@@ -1,6 +1,6 @@
 function init(){
   if($('teamfeed').length && $('teamfeed').html() == "")
-  var url = window.location.host + '/team'
+  var url = 'https://' + window.location.host + '/team'
   $.getJSON( url + '?format=json-pretty', function( data ) {
     console.log(data);
   })
