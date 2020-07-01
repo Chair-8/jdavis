2722d98a390055073512d202270a70908a4613e9
Joe Cory <hello@joecory.co.uk>
2020-07-01 22:17:04 +0100

    align classes

--- scripts/team.js
+++ scripts/team.js
@@ -13,7 +13,13 @@ function init(){
     console.log($(e.currentTarget))
     console.log($(e.currentTarget.offsetLeft)[0] + $(e.currentTarget.offsetWidth)[0])
     console.log($(window).width())
-    //if($(e.offsetLeft) + $(e.offsetWidth))
+    if($(e.currentTarget.offsetLeft)[0] + $(e.currentTarget.offsetWidth)[0] + $(e.currentTarget.offsetWidth)[0] + parseInt$(e.currentTarget).css('margin-right')) > $('teamfeed').width()){
+      //lightbox goes to the left
+      $(e.currentTarget).find('.teamItem_lightbox').addClass('rightAlign')
+    }else{
+      //lightbox goes to the right
+      $(e.currentTarget).find('.teamItem_lightbox').addClass('leftAlign')
+    }
     $(e.currentTarget).find('.teamItem_lightbox').css('width', (parseInt($(e.currentTarget).css('width')) * 2 + parseInt($(e.currentTarget).css('margin-right'))) + 'px')
     $(e.currentTarget).find('.teamItem_lightbox').css('height', parseInt($(e.currentTarget).css('height')) + 'px')
   })
