319f198173e67b8bfedb69d2a1c178892ac6a47c
Joe Cory <hello@joecory.co.uk>
2020-07-01 22:09:51 +0100

    spelling and width detection

--- scripts/team.js
+++ scripts/team.js
@@ -4,13 +4,16 @@ function init(){
   $.getJSON( url + '?format=json-pretty', function( data ) {
     console.log(data);
     data.items.forEach(function(e, i){
-      $('teamfeed').append('<div class="teamItem"><div class="teamItem_content"><div class="teamItem_image" style="backgrond-image:url('+e.assetUrl+')"></div><div class="teamItem_name">'+e.title+'</div><div class="teamItem_certifications">'+e.customContent.certifications.html+'</div><div class="teamItem_title">'+e.customContent.jobTitle.html+'</div></div><div class="teamItem_lightbox"><div class="teamItem_lightbox--name">'+e.title+'</div><div class="teamItem_lightbox--email">'+e.customContent.email+'</div><div class="teamItem_lightbox--bio">'+e.customContent.bio.html+'</div></div></div>')
+      $('teamfeed').append('<div class="teamItem"><div class="teamItem_content"><div class="teamItem_image" style="background-image:url('+e.assetUrl+')"></div><div class="teamItem_name">'+e.title+'</div><div class="teamItem_certifications">'+e.customContent.certifications.html+'</div><div class="teamItem_title">'+e.customContent.jobTitle.html+'</div></div><div class="teamItem_lightbox"><div class="teamItem_lightbox--name">'+e.title+'</div><div class="teamItem_lightbox--email">'+e.customContent.email+'</div><div class="teamItem_lightbox--bio">'+e.customContent.bio.html+'</div></div></div>')
     })
   })
 
   $('body').on('click', '.teamItem', function(e){
     console.log('clicked')
     console.log($(e.currentTarget))
+    console.log($(e.offsetLeft) + $(e.offsetWidth))
+    console.log($(window).width())
+    //if($(e.offsetLeft) + $(e.offsetWidth))
     $(e.currentTarget).find('.teamItem_lightbox').css('width', (parseInt($(e.currentTarget).css('width')) * 2 + parseInt($(e.currentTarget).css('margin-right'))) + 'px')
     $(e.currentTarget).find('.teamItem_lightbox').css('height', parseInt($(e.currentTarget).css('height')) + 'px')
   })
