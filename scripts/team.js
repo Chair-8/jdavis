3cbb08882fb0a41f6c555dc19aef0d80eacb20b0
Joe Cory <hello@joecory.co.uk>
2020-07-01 21:21:26 +0100

    team js update and checkboxes for cats

--- scripts/team.js
+++ scripts/team.js
@@ -3,6 +3,9 @@ function init(){
   var url = 'https://' + window.location.host + '/team'
   $.getJSON( url + '?format=json-pretty', function( data ) {
     console.log(data);
+    data.items.forEach(function(e, i){
+      $('teamfeed').append('<div class="teamItem">>div class="teamItem_content"><div class="teamItem_image" style="backgrond-image:url('+e.assetUrl+')"></div><div class="teamItem_name">'+e.title+'</div><div class="teamItem_certifications">'+e.customContent.certifications+'</div><div class="teamItem_title">'+e.customContent.jobTitle+'</div></div><div class="teamItem_lightbox"><div class="teamItem_lightbox--name">'+e.title+'</div><div class="teamItem_lightbox--email">'+e.customContent.email+'</div><div class="teamItem_lightbox--bio">'+e.customContent.bio+'</div></div></div>')
+    })
   })
 }
 init()
