cf204771af9b3723cf52f83aa2b5c041c79aab5c
Joe Cory <hello@joecory.co.uk>
2020-07-01 21:43:05 +0100

    on click

--- scripts/team.js
+++ scripts/team.js
@@ -7,5 +7,9 @@ function init(){
       $('teamfeed').append('<div class="teamItem"><div class="teamItem_content"><div class="teamItem_image" style="backgrond-image:url('+e.assetUrl+')"></div><div class="teamItem_name">'+e.title+'</div><div class="teamItem_certifications">'+e.customContent.certifications.html+'</div><div class="teamItem_title">'+e.customContent.jobTitle.html+'</div></div><div class="teamItem_lightbox"><div class="teamItem_lightbox--name">'+e.title+'</div><div class="teamItem_lightbox--email">'+e.customContent.email+'</div><div class="teamItem_lightbox--bio">'+e.customContent.bio.html+'</div></div></div>')
     })
   })
+
+  $('.teamItem').click(function(){
+    $(this).find('.teamItem_lightbox').style('width', $(this).css('width') * 2 + $(this).css('margin-right'))
+  })
 }
 init()
