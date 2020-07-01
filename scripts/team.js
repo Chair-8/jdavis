7370ab60d64dd1a4b6649ef77eac6140ce28b86c
Joe Cory <hello@joecory.co.uk>
2020-07-01 21:23:06 +0100

    fix >

--- scripts/team.js
+++ scripts/team.js
@@ -4,7 +4,7 @@ function init(){
   $.getJSON( url + '?format=json-pretty', function( data ) {
     console.log(data);
     data.items.forEach(function(e, i){
-      $('teamfeed').append('<div class="teamItem">>div class="teamItem_content"><div class="teamItem_image" style="backgrond-image:url('+e.assetUrl+')"></div><div class="teamItem_name">'+e.title+'</div><div class="teamItem_certifications">'+e.customContent.certifications+'</div><div class="teamItem_title">'+e.customContent.jobTitle+'</div></div><div class="teamItem_lightbox"><div class="teamItem_lightbox--name">'+e.title+'</div><div class="teamItem_lightbox--email">'+e.customContent.email+'</div><div class="teamItem_lightbox--bio">'+e.customContent.bio+'</div></div></div>')
+      $('teamfeed').append('<div class="teamItem"><div class="teamItem_content"><div class="teamItem_image" style="backgrond-image:url('+e.assetUrl+')"></div><div class="teamItem_name">'+e.title+'</div><div class="teamItem_certifications">'+e.customContent.certifications+'</div><div class="teamItem_title">'+e.customContent.jobTitle+'</div></div><div class="teamItem_lightbox"><div class="teamItem_lightbox--name">'+e.title+'</div><div class="teamItem_lightbox--email">'+e.customContent.email+'</div><div class="teamItem_lightbox--bio">'+e.customContent.bio+'</div></div></div>')
     })
   })
 }
