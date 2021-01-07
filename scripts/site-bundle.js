99289501d11f1e87fd74fb22cad23b4ef8a414bc
Joe <alex@managemywebsite.co.uk>
2021-01-07 13:49:16 +0000

    a

--- scripts/site-bundle.js
+++ scripts/site-bundle.js
@@ -163,8 +163,8 @@ function init(){
         filterValue = filterValue + '.casestudy'
       }else if($(this).text().replace(/[^a-zA-Z ]/g, "").replace(/\s/g, '').toLowerCase() == "projects"){
         filterValue = filterValue + '.project'
-      }else if($(this).text().replace(/[^a-zA-Z ]/g, "").replace(/\s/g, '').toLowerCase() == "commercialinteriors"){
-        filterValue = filterValue + '.commercial'
+      }else if($(this).text().replace(/[^a-zA-Z ]/g, "").replace(/\s/g, '').toLowerCase() == "commercial"){
+        filterValue = filterValue + '.commercialinteriors'
       }else{
         filterValue = filterValue + '.' + $(this).text().replace(/[^a-zA-Z ]/g, "").replace(/\s/g, '').toLowerCase()
       }
